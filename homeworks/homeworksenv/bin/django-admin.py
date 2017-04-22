7bd83fa00d0ea5cca9fe2d650be1704f22cd4833
Ivan Lakovic <laky55555@gmail.com>
2017-04-22 17:13:44 +0200

    Removed virtual env from source controle

--- homeworks/homeworksenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ivan/Dropbox/Faks/5_godina/application_security/homeworks/homeworksenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
